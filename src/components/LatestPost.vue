b3d849fd08638d906e34295915f47245cc52f97f
Rpx <ritaa.pinho@gmail.com>
2020-08-13 06:35:30 +0100

    added loading page

--- src/components/LatestPost.vue
+++ src/components/LatestPost.vue
@@ -2,23 +2,33 @@
   <section>
     <h2 class="mt-32 flex justify-center text-3xl ">The Happy Algorithm</h2>
     <transition mode="out-in">
-    <div class="grid grid-rows-4 grid-flow-col justify-center">
-      <div class=" text-center mt-8 mb-8 pl-6 pt-8 " v-for="(post, index) in posts" :key="index">
-        <router-link :to="{ name: 'post', params: { id: post.id } }">
-  
-          <img
+      <div
+        v-if="posts"
+        key="posts"
+        class="grid grid-rows-4 grid-flow-col justify-center"
+      >
+        <div
+          class=" text-center mt-8 mb-8 pl-6 pt-8 "
+          v-for="(post, index) in posts"
+          :key="index"
+        >
+          <router-link :to="{ name: 'post', params: { id: post.id } }">
+            <img
               class=" post-img rounded h-48 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 "
               :src="'/' + post.photo"
               :alt="post.title"
-          />
-  
-          <h2 class="post-title pt-4 flex justify-center">{{ post.title }}</h2>
-          <p class="text-justify text-sm pt-6 flex justify-center">
-            {{ post.description }}
-          </p>
-        </router-link>
+            />
+
+            <h2 class="post-title pt-4 flex justify-center">
+              {{ post.title }}
+            </h2>
+            <p class="text-justify text-sm pt-6 flex justify-center">
+              {{ post.description }}
+            </p>
+          </router-link>
+        </div>
       </div>
-    </div>
+      <LoadingPage key="loading" v-else />
     </transition>
   </section>
 </template>
@@ -35,9 +45,11 @@ export default {
   },
   methods: {
     getAllPosts() {
-      api.get(`/post`).then(response => {
-       return this.posts = response.data;
-      });
+      window.setTimeout(() => {
+        api.get(`/post`).then(response => {
+          return (this.posts = response.data);
+        });
+      }, 1500);
     },
   },
   created() {
@@ -46,8 +58,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/styles/tailwind.css";
+import LoadingPage from "./components/LoadingPage.vue";
 
+Vue.component("LoadingPage", LoadingPage);
 Vue.config.productionTip = false;
 
 new Vue({
